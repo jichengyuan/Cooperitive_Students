f8aea8dd3d0615cb3d3977c8f659c95b63def30e
jicheng <jicheng.yuan@tu-berlin.de>
2024-01-12 17:46:07 +0100

    bug_fix

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Evaluating..."
 config="configs/faster_rcnn_R50_BDD.yaml"
-weights = "./output/faster_rcnn_R50_BDD/model_best.pth"
-OUTPUT_DIR = "./output"
+weights="./output/faster_rcnn_R50_BDD/model_best.pth"
+OUTPUT_DIR="./output"
 
 python train.py \
        --config ${config} \
