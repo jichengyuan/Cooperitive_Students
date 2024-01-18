4b9a81bc0e0b6ef00c6ea836e38215a47b1a105c
jicheng <jicheng.yuan@tu-berlin.de>
2024-01-18 13:00:11 +0100

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,11 +4,9 @@ gpu_ratio=$(echo "$num_gpus / 2" | bc -l) # 2 is the default number of gpus per 
 echo "num_gpus: $num_gpus, default_gpus number is 2, so the gpu_ratio: $gpu_ratio"
 
 config="configs/faster_rcnn_R50_BDD.yaml"
-resume=false
 OUTPUT_DIR="./output"
 
 python train.py \
        --num-gpus ${num_gpus} \
        --config ${config} \
-       --resume ${resume} \
        OUTPUT_DIR ${OUTPUT_DIR}
